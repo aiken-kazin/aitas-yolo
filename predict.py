99983f791fe5ec23dfcba8c3d58b8dd6b85c34ae
aiken.kazin <aikenkazin@gmail.com>
2025-10-03 14:00:12 +0500

    ....

--- predict.py
+++ predict.py
@@ -1,45 +1,53 @@
+import json
 from ultralytics import YOLO
 
 
-
-MODEL_PATH = "/aitas-qcloudy/models/best.pt" 
+MODEL_PATH = "models/best.pt" 
 
 def predict(model_path, image_path):
-    # Load a model
+    # Load model
     model = YOLO(model_path)
     results = model.predict(
         source=image_path,
         imgsz=1280,          # change if you want
         conf=0.3,            # confidence threshold
         iou=0.5,             # NMS IoU
-        device=0,            # GPU 0; use 'cpu' if no GPU
+        device="cpu",        # GPU 0; use 'cpu' if no GPU
         save=True,           # save images with predictions
         show_labels=False,
         line_width=6,
-        project="results",   # your root folder
-        name="sunflower",    # subfolder inside project
-        # show=True          # display the image with predictions
+        project="results",   # root folder
+        name="sunflower",    # subfolder
+        # show=True
     )
+
+    output = []  # list to store JSON results for all images
 
     for i, r in enumerate(results):
         class_ids = r.boxes.cls.int().tolist()  # predicted class indices
 
         # initialize all classes with 0
-        counts = {cls_id: 0 for cls_id in model.names.keys()}
+        counts = {model.names[cls_id]: 0 for cls_id in model.names.keys()}
 
         # update counts
         for cls_id in class_ids:
-            counts[cls_id] += 1
+            counts[model.names[cls_id]] += 1
 
-        print(f"\nImage {i+1}: {r.path}")
-        for cls_id, cnt in counts.items():
-            class_name = model.names[cls_id]
-            print(f"  {class_name}: {cnt}")
+        # store per-image result in JSON structure
+        image_result = {
+            "image": r.path,
+            "counts": counts
+        }
+        output.append(image_result)
 
+    # Print JSON formatted output
+    print(json.dumps(output, indent=4, ensure_ascii=False))
+
+    return output
 
 
 if __name__ == "__main__":
-    image_path = "path_to_your_image.jpg"
+    image_path = "test_image/DJI_0674.JPG"
     predict(MODEL_PATH, image_path)
 
 
